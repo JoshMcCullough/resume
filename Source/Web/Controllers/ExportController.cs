232f674d7d32171cfda44824de8436017cba5504
Josh McCullough <joshua.mccullough@gmail.com>
2015-02-23 17:13:44 -0500

    Implemented PDF export (download, really).

--- Source/Web/Controllers/ExportController.cs
+++ Source/Web/Controllers/ExportController.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using QuantumConcepts.Common.Extensions;
 
 namespace JSM.Web.Controllers
 {
     public class ExportController : BaseController
     {
-        public ActionResult PDF()
-        {
+        private const string DownloadVirtualPath = "~/Content/Download/";
 
-            return null;
+        public FileResult PDF()
+        {
+            string downloadFilename = GetResumeDownloadFilename("pdf");
+
+            return this.File(Server.MapPath("{0}{1}".FormatString(ExportController.DownloadVirtualPath, "Resume.pdf")), MimeMapping.GetMimeMapping(downloadFilename), downloadFilename);
+        }
+
+        public static string GetResumeDownloadFilename(string extension) {
+            return "Josh McCullough's Resume {0}.{1}".FormatString(DateTime.Now, extension);
         }
     }
 }
